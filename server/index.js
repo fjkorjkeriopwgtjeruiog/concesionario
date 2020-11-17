77572ef66931cd2fc5e73ab4a3c4eb54a3ffac63
fjkorjkeriopwgtjeruiog <danielpareja1992@gmail.com>
2020-11-17 14:09:43 +0100

    Maestro

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader(
     'Access-Control-Allow-Headers',
